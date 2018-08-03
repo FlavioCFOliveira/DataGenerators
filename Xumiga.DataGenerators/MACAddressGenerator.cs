4834740770b1dfd5aef1596f26d25bb3b14b393e
FLAVIOCFO-LAPTO\o_mou <geral@flaviooliveira.com>
2018-08-03 16:03:32 +0100

    MAC address generator and tests

--- Xumiga.DataGenerators/MACAddressGenerator.cs
+++ Xumiga.DataGenerators/MACAddressGenerator.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Xumiga.DataGenerators
@@ -12,6 +13,24 @@ namespace Xumiga.DataGenerators
             rand = new Random();
         }
         public static Random rand;
+
+
+        public static string Generate(string separator = ":")
+        {
+            if (separator == null) separator = string.Empty;
+            
+            if (separator != ":" && separator != "-" && separator != "" && separator != string.Empty)
+            {
+                throw new ArgumentException("Invalid separator character", nameof(separator));
+            }
+
+            byte[] octets = new byte[6];
+            rand.NextBytes(octets);
+
+            string[] hexOctets = octets.Select(b => b.ToString("x2")).ToArray();
+
+            return string.Join(separator, hexOctets);
+        }
 
     }
 }
