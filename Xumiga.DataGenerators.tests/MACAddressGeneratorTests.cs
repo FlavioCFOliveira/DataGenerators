c3027d819089b34176b47dead5277a5c488272d7
FLAVIOCFO-LAPTO\o_mou <geral@flaviooliveira.com>
2018-08-03 11:38:04 +0100

    Base class structure for Class and tests

---
+++ Xumiga.DataGenerators.tests/MACAddressGeneratorTests.cs
@@ -0,0 +1,17 @@
+using System;
+using Xunit;
+using Xumiga.DataGenerators;
+
+namespace Xumiga.DataGenerator.tests
+{
+    public class MACAddressGeneratorTests
+    {
+
+        [Fact]
+        public void MACAddressGenerator_Generate_SUCCESS()
+        {
+
+        }
+        
+    }
+}
---
+++ Xumiga.DataGenerators/MACAddressGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xumiga.DataGenerators
+{
+    //https://en.wikipedia.org/wiki/MAC_address
+    public static class MACAddressGenerator
+    {
+        static MACAddressGenerator()
+        {
+            rand = new Random();
+        }
+        public static Random rand;
+
+    }
+}
