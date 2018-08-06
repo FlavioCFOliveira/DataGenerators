ea91dd185ec97aa473acb569f122a54b5dd6381b
FLAVIOCFO-LAPTO\o_mou <geral@flaviooliveira.com>
2018-08-06 09:40:23 +0100

    Hexadecimal color generator and tests

--- Xumiga.DataGenerators.tests/HexadecimalColorGeneratorTests.cs
+++ Xumiga.DataGenerators.tests/HexadecimalColorGeneratorTests.cs
@@ -6,11 +6,24 @@ namespace Xumiga.DataGenerator.tests
 {
     public class HexadecimalColorGeneratorTests
     {
+        const string HEXA_CHARS = "0123456789abcdef";
 
         [Fact]
         public void HexadecimalColorGenerator_Generate_SUCCESS()
         {
-            
+            string generated = HexadecimalColorGenerator.Generate();
+
+            Assert.NotNull(generated);
+            Assert.NotEmpty(generated);
+
+            Assert.StartsWith("#", generated);
+            Assert.True(generated.Length == 7);
+
+            for (int i = 1; i < generated.Length; i++)
+            {
+                Assert.True(HEXA_CHARS.Contains(generated[i]));
+            }
+
         }
 
     }
--- Xumiga.DataGenerators/HexadecimalColorGenerator.cs
+++ Xumiga.DataGenerators/HexadecimalColorGenerator.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Xumiga.DataGenerators
@@ -12,6 +13,20 @@ namespace Xumiga.DataGenerators
             rand = new Random();
         }
         public static Random rand;
+
+        /// <summary>
+        /// Generates a random string representing a color in hexadecimal format
+        /// </summary>
+        /// <returns>Hexadecimal color in the format #AAAAAA</returns>
+        public static string Generate()
+        {
+            byte[] buffer = new byte[3];
+            rand.NextBytes(buffer);
+
+            string[] hexBuffer = buffer.Select(b => b.ToString("x2")).ToArray();
+
+            return $"#{string.Join(string.Empty, hexBuffer)}";
+        }
 
 
     }
