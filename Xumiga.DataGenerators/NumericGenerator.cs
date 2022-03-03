586b2c727b5837601a46ddee16496cf4b6615077
Flávio CF Oliveira <geral@flaviooliveira.com>
2022-03-03 16:18:32 +0000

    documentation and file formatting

--- Xumiga.DataGenerators/NumericGenerator.cs
+++ Xumiga.DataGenerators/NumericGenerator.cs
@@ -1,75 +1,127 @@
-﻿namespace Xumiga.DataGenerators
+﻿using System;
+using System.Globalization;
+
+namespace Xumiga.DataGenerators;
+
+/// <summary>
+/// Random generator for numeric values
+/// </summary>
+public static class NumericGenerator
 {
-    using System;
-    using System.Globalization;
+    private static readonly Random rand;
+    private static readonly NumberFormatInfo culture;
 
-    public static class NumericGenerator
+    static NumericGenerator()
     {
-        private static readonly Random rand;
-        private static readonly NumberFormatInfo culture;
+        rand = new Random();
+        culture = NumberFormatInfo.CurrentInfo;
+    }
 
-        static NumericGenerator()
-        {
-            rand = new Random();
-            culture = NumberFormatInfo.CurrentInfo;
-        }
+    /// <summary>
+    /// returns a random int number
+    /// </summary>
+    /// <returns></returns>
+    public static int GenerateInteger()
+    {
+        return rand.Next();
+    }
 
-        public static int GenerateInteger()
-        {
-            return rand.Next();
-        }
-        public static int GenerateInteger(int maxValue)
-        {
-            return rand.Next(maxValue + 1);
-        }
-        public static int GenerateInteger(int minValue, int maxValue)
-        {
-            return rand.Next(minValue, maxValue + 1);
-        }
+    /// <summary>
+    /// returns a random int mumber below a givem value
+    /// </summary>
+    /// <param name="maxValue"></param>
+    /// <returns></returns>
+    public static int GenerateInteger(int maxValue)
+    {
+        return rand.Next(maxValue + 1);
+    }
 
-        public static byte GenerateByte()
-        {
-            byte[] r = new byte[1];
-            rand.NextBytes(r);
+    /// <summary>
+    /// returns a random number between two numbers
+    /// </summary>
+    /// <param name="minValue"></param>
+    /// <param name="maxValue"></param>
+    /// <returns></returns>
+    public static int GenerateInteger(int minValue, int maxValue)
+    {
+        return rand.Next(minValue, maxValue + 1);
+    }
 
-            return r[0];
-        }
+    /// <summary>
+    /// returns a ramdom byte
+    /// </summary>
+    /// <returns></returns>
+    public static byte GenerateByte()
+    {
+        byte[] r = new byte[1];
+        rand.NextBytes(r);
 
-        public static long GenerateLong()
-        {
-            return rand.NextLong();
-        }
-        public static long GenerateLong(long max)
-        {
-            return rand.NextLong(max);
-        }
-        public static long GenerateLong(long min, long max)
-        {
-            return rand.NextLong(min, max);
-        }
+        return r[0];
+    }
 
-        public static short GenerateShort(short minValue, short maxValue)
-        {
-            return (short)rand.Next((int)minValue, (int)maxValue);
-        }
+    /// <summary>
+    /// returns a long / int64 number
+    /// </summary>
+    /// <returns></returns>
+    public static long GenerateLong()
+    {
+        return rand.NextLong();
+    }
 
-        public static decimal GenerateDecimal(int precision, int scale)
-        {
-            if (precision == 0) throw new Exception("precision must be greater than zero.");
-            if (precision == scale) throw new Exception("precision and scale must have different values");
-            if (precision < scale) throw new Exception("precision cannot be less than scale");
+    /// <summary>
+    /// returns a long / int64 number below a guvem number
+    /// </summary>
+    /// <param name="max"></param>
+    /// <returns></returns>
+    public static long GenerateLong(long max)
+    {
+        return rand.NextLong(max);
+    }
 
-            int randPrecision = NumericGenerator.GenerateInteger(1, (precision - scale) + 1);
-            var a = StringGenerator.GetNumeric(randPrecision);
+    /// <summary>
+    /// returns a long / int64 number between two numbers
+    /// </summary>
+    /// <param name="min"></param>
+    /// <param name="max"></param>
+    /// <returns></returns>
+    public static long GenerateLong(long min, long max)
+    {
+        return rand.NextLong(min, max);
+    }
 
-            int randScale = NumericGenerator.GenerateInteger(1, scale + 1);
-            var b = StringGenerator.GetNumeric(randScale);
+    /// <summary>
+    /// returns a short / int16 between two numbers
+    /// </summary>
+    /// <param name="minValue"></param>
+    /// <param name="maxValue"></param>
+    /// <returns></returns>
+    public static short GenerateShort(short minValue, short maxValue)
+    {
+        return (short)rand.Next((int)minValue, (int)maxValue);
+    }
 
-            decimal result = decimal.Parse($"{a}{culture.NumberDecimalSeparator}{b}");
+    /// <summary>
+    /// returns a decimal value
+    /// </summary>
+    /// <param name="precision"></param>
+    /// <param name="scale"></param>
+    /// <returns></returns>
+    /// <exception cref="Exception"></exception>
+    public static decimal GenerateDecimal(int precision, int scale)
+    {
+        if (precision == 0) throw new Exception("precision must be greater than zero.");
+        if (precision == scale) throw new Exception("precision and scale must have different values");
+        if (precision < scale) throw new Exception("precision cannot be less than scale");
 
-            return result;
-        }
+        int randPrecision = NumericGenerator.GenerateInteger(1, (precision - scale) + 1);
+        var a = StringGenerator.GetNumeric(randPrecision);
 
+        int randScale = NumericGenerator.GenerateInteger(1, scale + 1);
+        var b = StringGenerator.GetNumeric(randScale);
+
+        decimal result = decimal.Parse($"{a}{culture.NumberDecimalSeparator}{b}");
+
+        return result;
     }
 
 }
--- Xumiga.DataGenerators/StringGenerator.cs
+++ Xumiga.DataGenerators/StringGenerator.cs
@@ -1,143 +1,253 @@
-﻿namespace Xumiga.DataGenerators
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Xumiga.DataGenerators;
+
+/// <summary>
+/// Random generator for character sequences
+/// </summary>
+public static class StringGenerator
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
+    private const string FULL_CHARS = @"qwertyuiopasdfghjklzxcvbnm0123456789QWERTYUIOPASDFGHJKLZXCVBNM@£§€{[]}|\!#$%&/()=?»*ªº/-_<>'~^.""";
+    private const string ALPHANUMERIC_CHARS = "qwertyuiopasdfghjklzxcvbnm0123456789QWERTYUIOPASDFGHJKLZXCVBNM";
+    private const string ALPHABETIC_CHARS = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
+    private const string ALPHABETIC_UPPER_CHARS = "QWERTYUIOPASDFGHJKLZXCVBNM";
+    private const string ALPHABETIC_LOWER_CHARS = "qwertyuiopasdfghjklzxcvbnm";
+    private const string NUMERIC_CHARS = "0123456789";
+    private const string SYMBOL_CHARS = @"@£§€{[]}|\!#$%&/()=?»*ªº/-_<>'~^.""";
+    private static readonly Random rand;
 
-    public static class StringGenerator
+    static StringGenerator()
     {
-        private const string FULL_CHARS = @"qwertyuiopasdfghjklzxcvbnm0123456789QWERTYUIOPASDFGHJKLZXCVBNM@£§€{[]}|\!#$%&/()=?»*ªº/-_<>'~^.""";
-        private const string ALPHANUMERIC_CHARS = "qwertyuiopasdfghjklzxcvbnm0123456789QWERTYUIOPASDFGHJKLZXCVBNM";
-        private const string ALPHABETIC_CHARS = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
-        private const string ALPHABETIC_UPPER_CHARS = "QWERTYUIOPASDFGHJKLZXCVBNM";
-        private const string ALPHABETIC_LOWER_CHARS = "qwertyuiopasdfghjklzxcvbnm";
-        private const string NUMERIC_CHARS = "0123456789";
-        private const string SYMBOL_CHARS = @"@£§€{[]}|\!#$%&/()=?»*ªº/-_<>'~^.""";
-        private static readonly Random rand;
-
-        static StringGenerator()
-        {
-            rand = new Random();
-        }
-
-        public static string GetFromSource(int outputSize, string sourceChars)
-        {
-            return string.Create(outputSize, string.Empty, (span, v) =>
-            {
-                for (int i = 0; i < outputSize; i++)
-                {
-                    span[i] = sourceChars[rand.Next(sourceChars.Length)];
-                }
-            });
-        }
-        public static string GetAlphaNumeric(int outputSize)
-        {
-            return GetFromSource(outputSize, ALPHANUMERIC_CHARS);
-        }
-        public static string GetAlphaNumericWithSymbols(int outputSize)
-        {
-            return GetFromSource(outputSize, FULL_CHARS);
-        }
-        public static string GetNumeric(int outputSize)
-        {
-            return GetFromSource(outputSize, NUMERIC_CHARS);
-        }
-        public static string GetSymbols(int outputSize)
-        {
-            return GetFromSource(outputSize, SYMBOL_CHARS);
-        }
-        public static string GetAlphabeticUpper(int outputSize)
-        {
-            return GetFromSource(outputSize, ALPHABETIC_UPPER_CHARS);
-        }
-        public static string GetAlphabeticLower(int outputSize)
-        {
-            return GetFromSource(outputSize, ALPHABETIC_LOWER_CHARS);
-        }
-        public static string GetAlphabetic(int outputSize)
-        {
-            return GetFromSource(outputSize, ALPHABETIC_CHARS);
-        }
-        public static string GetAlphaNumeric(int minSize, int maxSize)
-        {
-            return GetAlphaNumeric(rand.Next(minSize, maxSize));
-        }
-        public static string GetAlphaNumericWithSymbols(int minSize, int maxSize)
-        {
-            return GetAlphaNumericWithSymbols(rand.Next(minSize, maxSize));
-        }
-        public static string GetNumeric(int minSize, int maxSize)
-        {
-            return GetNumeric(rand.Next(minSize, maxSize));
-        }
-        public static string GetSymbols(int minSize, int maxSize)
-        {
-            return GetSymbols(rand.Next(minSize, maxSize));
-        }
-        public static string GetAlphabeticUpper(int minSize, int maxSize)
-        {
-            return GetAlphabeticUpper(rand.Next(minSize, maxSize));
-        }
-        public static string GetAlphabeticLower(int minSize, int maxSize)
-        {
-            return GetAlphabeticLower(rand.Next(minSize, maxSize));
-        }
-        public static string GetAlphabetic(int minSize, int maxSize)
-        {
-            return GetAlphabetic(rand.Next(minSize, maxSize));
-        }
-        public static string GenerateStrongString(int size, int minNumbers = 0, int minUpperChars = 0, int minLowerChars = 0, int minSymbolChars = 0)
-        {
-            var minRequiredTotal = (minNumbers + minUpperChars + minLowerChars + minSymbolChars);
-            if (size < minRequiredTotal) throw new ArgumentException("Size must be greater than the sum of all minimum char parameters");
-
-            List<string> tmp = new List<string>();
-
-            if (minNumbers > 0) { tmp.Add(GetNumeric(minNumbers)); }
-            if (minUpperChars > 0) { tmp.Add(GetAlphabeticUpper(minUpperChars)); }
-            if (minLowerChars > 0) { tmp.Add(GetAlphabeticLower(minLowerChars)); }
-            if (minSymbolChars > 0) { tmp.Add(GetSymbols(minSymbolChars)); }
-
-            // random filler
-            var fillingSize = (size - minRequiredTotal);
-            if (fillingSize > 0) { tmp.Add(GetAlphaNumeric(fillingSize)); }
-
-            string joined = string.Join("", tmp);
-
-            string shuffled = new string(joined.ToCharArray().OrderBy(o => rand.Next()).ToArray());
-
-            return shuffled;
-        }
-
-        /// <summary>
-        /// Geta a random string using a mask to specify the alfabetica and numeric Chars example "XAANNXAANANNANANA"
-        /// A - Alfabetic [A-Z], N - Numeric [0-9], X - Alfanumeric[A-Z0-9] 
-        /// </summary>
-        /// <param name="mask">The mast</param>
-        /// <returns></returns>
-        public static string GetMaskedAlphaNumeric(string mask)
-        {
-            if (!string.IsNullOrEmpty(mask))
-            {
-                var result = new char[mask.Length];
-                for (int i = 0; i < mask.Length; i++)
-                {
-                    switch (mask[i])
-                    {
-                        case 'a': result[i] = GetAlphabeticLower(1)[0]; break;
-                        case 'A': result[i] = GetAlphabeticUpper(1)[0]; break;
-                        case 'n': result[i] = GetNumeric(1)[0]; break;
-                        case 'N': result[i] = GetNumeric(1)[0]; break;
-                        case 'X': result[i] = GetAlphaNumeric(1)[0]; break;
-                        case 'x': result[i] = GetAlphaNumeric(1)[0]; break;
-                        default: result[i] = mask[i]; break;
-                    }
-                }
-                return new string(result);
-            }
-
-            return string.Empty;
-        }
-
+        rand = new Random();
     }
+
+    /// <summary>
+    /// returns a random sequence of characters based on a source string
+    /// </summary>
+    /// <param name="outputSize">output sequence size</param>
+    /// <param name="sourceChars">string containing all the chars to be used</param>
+    /// <returns></returns>
+    public static string GetFromSource(int outputSize, string sourceChars)
+    {
+        return string.Create(outputSize, string.Empty, (span, v) =>
+        {
+            for (int i = 0; i < outputSize; i++)
+            {
+                span[i] = sourceChars[rand.Next(sourceChars.Length)];
+            }
+        });
+    }
+
+    /// <summary>
+    /// returns a random sequence of letters and numbers 
+    /// </summary>
+    /// <param name="outputSize"></param>
+    /// <returns></returns>
+    public static string GetAlphaNumeric(int outputSize)
+    {
+        return GetFromSource(outputSize, ALPHANUMERIC_CHARS);
+    }
+
+    /// <summary>
+    /// returns a random sequence of letters, numbers and symbols 
+    /// </summary>
+    /// <param name="outputSize"></param>
+    /// <returns></returns>
+    public static string GetAlphaNumericWithSymbols(int outputSize)
+    {
+        return GetFromSource(outputSize, FULL_CHARS);
+    }
+
+    /// <summary>
+    /// returns a random sequence of numbers
+    /// </summary>
+    /// <param name="outputSize"></param>
+    /// <returns></returns>
+    public static string GetNumeric(int outputSize)
+    {
+        return GetFromSource(outputSize, NUMERIC_CHARS);
+    }
+
+    /// <summary>
+    /// returns a random sequence of symbols
+    /// </summary>
+    /// <param name="outputSize"></param>
+    /// <returns></returns>
+    public static string GetSymbols(int outputSize)
+    {
+        return GetFromSource(outputSize, SYMBOL_CHARS);
+    }
+
+    /// <summary>
+    /// returns a random sequence of letters in uppercase
+    /// </summary>
+    /// <param name="outputSize"></param>
+    /// <returns></returns>
+    public static string GetAlphabeticUpper(int outputSize)
+    {
+        return GetFromSource(outputSize, ALPHABETIC_UPPER_CHARS);
+    }
+
+    /// <summary>
+    /// returns a random sequence of letters in lowercase 
+    /// </summary>
+    /// <param name="outputSize"></param>
+    /// <returns></returns>
+    public static string GetAlphabeticLower(int outputSize)
+    {
+        return GetFromSource(outputSize, ALPHABETIC_LOWER_CHARS);
+    }
+
+    /// <summary>
+    /// returns a random sequence of letters upper and lowe case 
+    /// </summary>
+    /// <param name="outputSize"></param>
+    /// <returns></returns>
+    public static string GetAlphabetic(int outputSize)
+    {
+        return GetFromSource(outputSize, ALPHABETIC_CHARS);
+    }
+
+    /// <summary>
+    /// returns a random sequence of numbers 
+    /// </summary>
+    /// <param name="minSize"></param>
+    /// <param name="maxSize"></param>
+    /// <returns></returns>
+    public static string GetAlphaNumeric(int minSize, int maxSize)
+    {
+        return GetAlphaNumeric(rand.Next(minSize, maxSize));
+    }
+
+    /// <summary>
+    /// returns a random sequence of letters, numbers or symbols 
+    /// </summary>
+    /// <param name="minSize"></param>
+    /// <param name="maxSize"></param>
+    /// <returns></returns>
+    public static string GetAlphaNumericWithSymbols(int minSize, int maxSize)
+    {
+        return GetAlphaNumericWithSymbols(rand.Next(minSize, maxSize));
+    }
+
+    /// <summary>
+    /// returns a random sequence of munbers 
+    /// </summary>
+    /// <param name="minSize"></param>
+    /// <param name="maxSize"></param>
+    /// <returns></returns>
+    public static string GetNumeric(int minSize, int maxSize)
+    {
+        return GetNumeric(rand.Next(minSize, maxSize));
+    }
+
+    /// <summary>
+    /// returns a random sequence of symbols 
+    /// </summary>
+    /// <param name="minSize"></param>
+    /// <param name="maxSize"></param>
+    /// <returns></returns>
+    public static string GetSymbols(int minSize, int maxSize)
+    {
+        return GetSymbols(rand.Next(minSize, maxSize));
+    }
+
+    /// <summary>
+    /// returns a random sequence of letters in upper case 
+    /// </summary>
+    /// <param name="minSize"></param>
+    /// <param name="maxSize"></param>
+    /// <returns></returns>
+    public static string GetAlphabeticUpper(int minSize, int maxSize)
+    {
+        return GetAlphabeticUpper(rand.Next(minSize, maxSize));
+    }
+
+    /// <summary>
+    /// returns a random sequence of letters in lower case
+    /// </summary>
+    /// <param name="minSize"></param>
+    /// <param name="maxSize"></param>
+    /// <returns></returns>
+    public static string GetAlphabeticLower(int minSize, int maxSize)
+    {
+        return GetAlphabeticLower(rand.Next(minSize, maxSize));
+    }
+
+    /// <summary>
+    /// returns a random sequence of letters in upper and lower case
+    /// </summary>
+    /// <param name="minSize"></param>
+    /// <param name="maxSize"></param>
+    /// <returns></returns>
+    public static string GetAlphabetic(int minSize, int maxSize)
+    {
+        return GetAlphabetic(rand.Next(minSize, maxSize));
+    }
+
+    /// <summary>
+    /// returns a random sequence of characters with an ammount of specific characters types
+    /// </summary>
+    /// <param name="size"></param>
+    /// <param name="minNumbers"></param>
+    /// <param name="minUpperChars"></param>
+    /// <param name="minLowerChars"></param>
+    /// <param name="minSymbolChars"></param>
+    /// <returns></returns>
+    /// <exception cref="ArgumentException"></exception>
+    public static string GenerateStrongString(int size, int minNumbers = 0, int minUpperChars = 0, int minLowerChars = 0, int minSymbolChars = 0)
+    {
+        var minRequiredTotal = (minNumbers + minUpperChars + minLowerChars + minSymbolChars);
+        if (size < minRequiredTotal) throw new ArgumentException("Size must be greater than the sum of all minimum char parameters");
+
+        List<string> tmp = new List<string>();
+
+        if (minNumbers > 0) { tmp.Add(GetNumeric(minNumbers)); }
+        if (minUpperChars > 0) { tmp.Add(GetAlphabeticUpper(minUpperChars)); }
+        if (minLowerChars > 0) { tmp.Add(GetAlphabeticLower(minLowerChars)); }
+        if (minSymbolChars > 0) { tmp.Add(GetSymbols(minSymbolChars)); }
+
+        // random filler
+        var fillingSize = (size - minRequiredTotal);
+        if (fillingSize > 0) { tmp.Add(GetAlphaNumeric(fillingSize)); }
+
+        string joined = string.Join("", tmp);
+
+        string shuffled = new string(joined.ToCharArray().OrderBy(o => rand.Next()).ToArray());
+
+        return shuffled;
+    }
+
+    /// <summary>
+    /// Geta a random string using a mask to specify the alfabetica and numeric Chars example "XAANNXAANANNANANA"
+    /// A - Alfabetic [A-Z], N - Numeric [0-9], X - Alfanumeric[A-Z0-9] 
+    /// </summary>
+    /// <param name="mask">The mast</param>
+    /// <returns></returns>
+    public static string GetMaskedAlphaNumeric(string mask)
+    {
+        if (!string.IsNullOrEmpty(mask))
+        {
+            var result = new char[mask.Length];
+            for (int i = 0; i < mask.Length; i++)
+            {
+                switch (mask[i])
+                {
+                    case 'a': result[i] = GetAlphabeticLower(1)[0]; break;
+                    case 'A': result[i] = GetAlphabeticUpper(1)[0]; break;
+                    case 'n': result[i] = GetNumeric(1)[0]; break;
+                    case 'N': result[i] = GetNumeric(1)[0]; break;
+                    case 'X': result[i] = GetAlphaNumeric(1)[0]; break;
+                    case 'x': result[i] = GetAlphaNumeric(1)[0]; break;
+                    default: result[i] = mask[i]; break;
+                }
+            }
+            return new string(result);
+        }
+
+        return string.Empty;
+    }
+
 }
